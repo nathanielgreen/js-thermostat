49ee312e6e69e3dffa4641b4dffd4334873e4baf
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-09-29 15:33:24 +0100

    Added tests for increasing/decreasing max/min temps and powersave, Tests passing.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,7 +1,36 @@
 describe('thermostat', function() {
 
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
   it('starts at 20 degrees', function() {
-    thermostat = new Thermostat;
     expect(thermostat.temperature).toEqual(20)
   });
+
+  it('can increase its temperature', function() {
+    thermostat.tempIncrease(3);
+    expect(thermostat.temperature).toEqual(23)
+  });
+
+  it('can decrease its temeperature', function() {
+    thermostat.tempDecrease(10);
+    expect(thermostat.temperature).toEqual(10)
+  });
+
+  it('has a minimum temeperature of 10 degrees', function() {
+    thermostat.tempDecrease(100)
+    expect(thermostat.temperature).toEqual(10)
+    // expect(thermostat.tempDecrease(100)).toThrow(new Error("The minimum temperature is 10"))
+  });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.powerSave).toBe(true)
+  });
+
+  it('has max temp 25 degrees if power saving mode is on', function() {
+    thermostat.tempIncrease(100)
+    expect(thermostat.temperature).toEqual(25)
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,20 @@
 function Thermostat() {
   this.temperature = 20
+  this.powerSave = true
+};
+
+Thermostat.prototype.tempIncrease = function(num) {
+  this.temperature += num;
+  if (this.powerSave == true) {
+    if (this.temperature > 25) {
+        this.temperature = 25
+    }
+  }
+};
+
+Thermostat.prototype.tempDecrease = function(num) {
+  this.temperature -= num;
+  if (this.temperature < 10) {
+      this.temperature = 10;
+  }
 };
