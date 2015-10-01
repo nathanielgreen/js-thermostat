fb60b98cc223814c6fb5e1d7fea231ebcecd550d
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-10-01 16:38:30 +0100

    Cleaned up the look of the codes, shortened lines. Changed look of the page, added images and tweaked colours. Removed redundant code.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ console.log("app is working")
 
 var thermostat = new Thermostat();
 
-
-
 var showTempVar = function() {
   $("#temp").text(thermostat.temperature + "ºC");
   if (thermostat.displayColour == 'Low Temp') {
@@ -19,21 +17,6 @@ var showTempVar = function() {
 }
 
 showTempVar();
-// var changeColourVar = function() {
-//   $("#temp").text(thermostat.temperature);
-//   if (thermostat.displayColour == 'Low Temp') {
-//     $('#temp').removeClass('Yellow Red');
-//     $('#temp').addClass('Green');
-//   } else if (thermostat.displayColour == 'Mid Temp') {
-//     $('#temp').removeClass('Green Red');
-//     $('#temp').addClass('Yellow')
-//   } else if (thermostat.displayColour == 'High Temp') {
-//     $('#temp').removeClass('Green Yellow');
-//     $('#temp').addClass('Red')
-//   }
-// }
-
-
 
 $('#tempUp').click(function() {
   thermostat.tempIncrease(1);
@@ -57,20 +40,22 @@ $('#powerSave').change(function() {
 
 var chosenCity = window.location.search.substring(11)
 
-var connectedLink = "http://api.openweathermap.org/data/2.5/weather?q=" + chosenCity;
-console.log(window.location)
-console.log(window.location.search)
-console.log(window.location.search.substring(2))
-
-console.log(window.location.search.substring(11))
-console.log(connectedLink)
+var connectedLink =
+  "http://api.openweathermap.org/data/2.5/weather?q="
+  + chosenCity;
 
 var myjson;
+
 $.getJSON(connectedLink, function(json){
     myjson = json;
 
   function showWeather(){
-      document.getElementById("showWeather").innerHTML = "The temperature in " + chosenCity + " is: " + JSON.stringify(Math.round(myjson.main.temp) - 273) + "ºC";
+      document.getElementById("showWeather").innerHTML =
+      "The temperature in "
+      + chosenCity
+      + " is: "
+      + JSON.stringify(Math.round(myjson.main.temp) - 273)
+      + "ºC";
   };
 
   showWeather();
--- spec/jquerySpec.js
+++
@@ -1,5 +0,0 @@
-describe('Thermostat', function() {
-  beforeEach(function() {
-
-  });
-})
